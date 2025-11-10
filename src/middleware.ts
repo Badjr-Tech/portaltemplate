8c55cbe08e3c67519e80d5a741fb9381513d2cfd
dakotah jennifer <dakotahjennifer@gmail.com>
2025-11-10 17:01:26 -0500

    fix: revert middleware explicit redirect, use auth.protect()

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from 'next/server'; // Import NextResponse
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
-    // Check if user is not authenticated
-    if (!auth().userId) { // auth() is a function that returns the auth state
-      const signInUrl = new URL('/sign-in', request.url);
-      return NextResponse.redirect(signInUrl);
-    }
+    auth.protect();
   }
 });
 
