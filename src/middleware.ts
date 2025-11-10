7b7718beb092a91daeaac11e3f0c273535a9b535
dakotah jennifer <dakotahjennifer@gmail.com>
2025-11-10 17:36:37 -0500

    debug: add console logs to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware((auth, request) => {
+  console.log('Middleware running for:', request.url);
+  console.log('Is public route:', isPublicRoute(request));
+
   if (!isPublicRoute(request)) {
+    console.log('Route is protected. Calling auth.protect()');
     auth.protect();
   }
 });
