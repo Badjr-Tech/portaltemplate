785ec40bd725c719bb16f9d89a0f92201e44952c
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-12 11:46:35 -0500

    Reintroduce Clerk SignIn component to sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
 export default function Page() {
   return (
     <div style={{ backgroundColor: 'lightblue', border: '2px solid red', padding: '20px' }}>
-      <h1>Sign In Page Test</h1>
-      <p>If you see this, the page is rendering!</p>
+      <SignIn />
     </div>
   );
 }
