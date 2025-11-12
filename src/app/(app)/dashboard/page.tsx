cca0f106c7e296a7d9918e997e9cfa221093fbbd
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-12 16:50:07 -0500

    fix: add protection to dashboard page

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,4 +1,11 @@
-export default function DashboardPage() {
+import { auth, redirectToSignIn } from '@clerk/nextjs/server';
+
+export default async function DashboardPage() {
+  const { userId } = await auth();
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
   return (
     <div className="bg-accent p-4">
       <h1 className="text-2xl font-bold">Dashboard</h1>
