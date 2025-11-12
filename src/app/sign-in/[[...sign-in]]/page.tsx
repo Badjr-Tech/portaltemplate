6c92c758573210d703e3b2393fd55eb1e01d8c69
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-11 20:47:44 -0500

    Fix: Add diagnostic styles to SignIn page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div style={{ backgroundColor: 'lightblue', border: '2px solid red', padding: '20px' }}>
+      <SignIn />
+    </div>
+  );
 }
