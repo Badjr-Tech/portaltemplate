98425506e37939ee5940feda544e4dea50d4a85a
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-12 16:43:38 -0500

    chore: add red border to h1 for deployment check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the Portal!</h1>
+      <h1 className="border-4 border-red-500">Welcome to the Portal!</h1>
       {userId ? (
         <p>You are signed in as user: {userId}</p>
       ) : (
