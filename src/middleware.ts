8514655fcd55ac7017f3b7d19ec75b20e2017256
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-12 16:14:37 -0500

    Switch to authMiddleware and define publicRoutes for sign-in/sign-up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect();
-  }
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)"],
 });
 
 export const config = {
-  matcher: [
-    '/((?!.+\.[\w]+$|_next).*), // Match all paths except static files and Next.js internal paths
-    '/', // Explicitly match the root
-    '/dashboard(.*)', // Explicitly match dashboard and its sub-paths
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)"],
 };
