ddc147f82a9a3d83ae813a0aee7afe3efa9b9b2d
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-12 12:13:52 -0500

    Refactor middleware.ts to use publicRoutes option directly in clerkMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,9 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth.protect();
-  }
+export default clerkMiddleware({
+  publicRoutes: ["/sign-in(.*)", "/sign-up(.*)"],
 });
 
 export const config = {
-  matcher: ['/', '/dashboard(.*)', '/((?!.+\\.[\\w]+$|_next).*)'],
+  matcher: ['/((?!.+\.[\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
